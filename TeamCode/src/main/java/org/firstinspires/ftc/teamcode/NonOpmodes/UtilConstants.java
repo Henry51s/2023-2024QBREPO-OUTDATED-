f79f689346e38bf1a3216fb732424ee7de9c3250
Henry51s <quochenry06@gmail.com>
2023-09-08 19:07:42 -0400

    Added new constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonOpmodes/UtilConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonOpmodes/UtilConstants.java
@@ -7,14 +7,22 @@ public class UtilConstants {
 
     //TO-DO: List config names of motors, servos, camera
 
-    //Webcam Constants-------------
-    public static final int xWidth = 800;
-    public static final int yWidth = 448;
+    //Vision Constants-------------
+    public static final int xWidth = 800; //Webcam Resolution X
+    public static final int yWidth = 448; //Webcam Resolution Y
 
     public static final int dashboardStreamFps = 5;
+
+    public static final int lowerH = 20;
+    public static final int lowerS = 70; //Lower bound HSV values for desired color
+    public static final int lowerV = 80;
+
+    public static final int upperH = 32;
+    public static final int upperS = 255; //Upper bound HSV values for desired color
+    public static final int upperV = 255;
     //-----------------------------
 
-    //PID Constants for TestPlatform Opmode
+    //PID Constants for TestPlatform OpMode
     public static double kP = 0;
     public static double kI = 0;
     public static double kD = 0;
