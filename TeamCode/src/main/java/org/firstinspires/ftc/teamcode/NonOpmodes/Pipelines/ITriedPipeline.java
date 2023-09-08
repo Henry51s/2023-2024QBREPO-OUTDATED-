170eea9f745822003fa219dcb1de915a337ec506
Henry51s <quochenry06@gmail.com>
2023-09-08 19:07:30 -0400

    ITriedPipeline W.I.P

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonOpmodes/Pipelines/ITriedPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonOpmodes/Pipelines/ITriedPipeline.java
@@ -2,18 +2,26 @@ package org.firstinspires.ftc.teamcode.NonOpmodes.Pipelines;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
 import org.openftc.easyopencv.OpenCvPipeline;
+import static org.firstinspires.ftc.teamcode.NonOpmodes.UtilConstants.*;
 
 public class ITriedPipeline extends OpenCvPipeline {
 
+    //Courtesy of ChatGPT
     Mat hsvImage = new Mat();
     Mat mask = new Mat();
 
-    Scalar lower = new Scalar(20, 70, 80); // HSV threshold bounds
-    Scalar upper = new Scalar(32, 255, 255);
+    Scalar lower = new Scalar(lowerH, lowerS, lowerV); // HSV threshold bounds
+    Scalar upper = new Scalar(upperH, upperS, upperV);
+    Scalar circleColor = new Scalar(0, 100, 100);
+
+    double xOffset = 0;
+    double yOffset = 0;
+    double distance = 0;
 
     @Override
     public Mat processFrame(Mat input) {
@@ -26,9 +34,21 @@ public class ITriedPipeline extends OpenCvPipeline {
         double m10 = moments.m10; //Measure of the object's horizontal information
         double m01 = moments.m01; //Measure of the object's vertical information
 
-        double xPos = (int) m10 / m00;
-        double yPos = (int) m01 / m00;
+        xOffset = (int) m10 / m00;
+        yOffset = (int) m01 / m00;
+        distance = Math.sqrt(Math.pow(xOffset,2)+Math.pow(yOffset,2));
+
 
         return null;
+    }
+
+    public double getX(){
+        return xOffset;
+    }
+    public double getY(){
+        return yOffset;
+    }
+    public double getDistance(){
+        return distance;
     }
 }
