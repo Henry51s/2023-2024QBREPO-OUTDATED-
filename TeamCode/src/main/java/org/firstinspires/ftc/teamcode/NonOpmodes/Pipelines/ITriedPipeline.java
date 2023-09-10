58842075f5e5c1a8c84f4b85ba6f2f79dde2e94f
Henry51s <quochenry06@gmail.com>
2023-09-10 17:41:57 -0400

    ITriedPipeline should be good

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonOpmodes/Pipelines/ITriedPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NonOpmodes/Pipelines/ITriedPipeline.java
@@ -1,42 +1,69 @@
 package org.firstinspires.ftc.teamcode.NonOpmodes.Pipelines;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
 import org.openftc.easyopencv.OpenCvPipeline;
-import static org.firstinspires.ftc.teamcode.NonOpmodes.UtilConstants.*;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ITriedPipeline extends OpenCvPipeline {
+    /*
+          (0,0) ----------- (Width, 0)
+            |                 |
+            |                 |
+            |                 |
+            |                 |
+            |                 |
+            |                 |
+            (0, Height) ----- (Width, Height)
+    */
+    //Dimensions for region of interest rectangle
+    int x1 = 0;
+    int y1 = 0;
+    int w = 200;
+    int h = 448;
 
-    //Courtesy of ChatGPT
-    Mat hsvImage = new Mat();
-    Mat mask = new Mat();
+    //Lower bound HSV values for desired color
+    int lowerH = 73;
+    int lowerS = 160;
+    int lowerV = 59;
 
-    Scalar lower = new Scalar(lowerH, lowerS, lowerV); // HSV threshold bounds
-    Scalar upper = new Scalar(upperH, upperS, upperV);
+    //Upper bound HSV values for desired color
+    int upperH = 255;
+    int upperS = 188;
+    int upperV = 98;
+
+    public Scalar lower = new Scalar(lowerH, lowerS, lowerV);
+    public Scalar upper = new Scalar(upperH, upperS, upperV);
 
     double xOffset = 0;
     double yOffset = 0;
 
+    Mat ycrcbImage = new Mat();
+    Mat mask = new Mat();
+    Mat hierarchy = new Mat();
+    Mat roi;
 
-    @Override
-    public Mat processFrame(Mat input) {
+    List<MatOfPoint> contours = new ArrayList<>();
 
-        Imgproc.cvtColor(input, hsvImage, Imgproc.COLOR_RGB2HSV);
-        Core.inRange(hsvImage,lower,upper,mask);
+    Rect boundingBox;
+    public Rect regionOfInterest = new Rect(x1,y1,w,h);
 
-        Moments moments = Imgproc.moments(mask, true);
-        double m00 = moments.m00; //Measure of the object's area
-        double m10 = moments.m10; //Measure of the object's horizontal information
-        double m01 = moments.m01; //Measure of the object's vertical information
-
-        xOffset = (int) m10 / m00;
-        yOffset = (int) m01 / m00;
-
-        return mask;
+    //Code for EOCV Sim
+    Telemetry telemetry;
+    public ITriedPipeline(Telemetry telemetry){
+        this.telemetry = telemetry;
     }
+
 
     public double getX(){
         return xOffset;
@@ -45,4 +72,49 @@ public class ITriedPipeline extends OpenCvPipeline {
         return yOffset;
     }
 
+    @Override
+    public Mat processFrame(Mat input) {
+        /*
+        1. Crop input image
+        2. Convert cropped image into yCrCb color space
+        3. Find the contours of the cropped image, put that info into a new "mask" mat
+        4. Clone the mask to another mat
+        5. Create a bounding box around the contour
+        6. Output bounding box x and y position
+        7. Profit
+        */
+
+        //Crops image
+        roi = new Mat(input, regionOfInterest);
+
+        //Converts image to yCrCb color space
+        Imgproc.cvtColor(roi, ycrcbImage, Imgproc.COLOR_RGB2YCrCb);
+
+        //Creates mask using given lower and upper bounds of color
+        Core.inRange(ycrcbImage,lower,upper,mask);
+
+        //Finds and draws contours around objects
+        Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.drawContours(mask, contours, -1, new Scalar(25,165,0), 2);
+
+        //Creates a rectangle around each object
+        for (MatOfPoint contours : contours){
+            boundingBox = Imgproc.boundingRect(contours);
+        }
+
+        //Draws rectangle on mask to be displayed
+        Imgproc.rectangle(mask, boundingBox, new Scalar(25,165,0), 2);
+
+        xOffset = boundingBox.x + (0.5* boundingBox.width);
+        yOffset = boundingBox.y + (0.5* boundingBox.height);
+
+        telemetry.addData("xOffset: ", xOffset);
+        telemetry.addData("yOffset: ", yOffset);
+        telemetry.update();
+
+        input.release();
+        roi.release();
+        ycrcbImage.release();
+        return mask;
+    }
 }
